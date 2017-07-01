3378dfd09387a63673c9a97d749c8f66e5232b26
Tristan Teufel <tristanteufel@googlemail.com>
2017-07-01 14:47:57 +0200

    fix output path

--- webpack/base.js
+++ webpack/base.js
@@ -7,7 +7,7 @@ module.exports = {
         gui: './src/index.js',
     },
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, '../dist'),
         filename: '[name].js'
     },
     module: {
