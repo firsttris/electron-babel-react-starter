b83904feba6aed261a6f5a69cdb65f643b622e11
firsttris <tristanteufel@googlemail.com>
2017-07-01 20:45:30 +0200

    redux dev tools

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,13 @@
-import {createStore, applyMiddleware} from 'redux'
+import {createStore, applyMiddleware, compose } from 'redux'
 import rootReducer from '../reducers/index'
 import thunk from 'redux-thunk';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const configureStore = (preloadedState) => createStore(
     rootReducer,
     preloadedState,
-    applyMiddleware(thunk)
+    composeEnhancers(applyMiddleware(thunk))
 );
 
 export default configureStore
